b87feb6f2a7238c20b5bfa87e37e8f92a9624ce1
nao-pon <hypweb@gmail.com>
2025-11-09 10:26:47 +0900

    Revert "Merge branch 'fix_login' into edge"
    This reverts commit 685c7204794cc890d0e7ac24e22b4c1d1335effa, reversing
    changes made to b68b01342266bc3a53c00a6360abb8838766abed.

--- custom_components/alexa_media/__init__.py
+++ custom_components/alexa_media/__init__.py
@@ -453,7 +453,7 @@ async def setup_alexa(hass, config_entry, login_obj: AlexaLogin):
             AlexaAPI.get_dnd_state(login_obj),
         ]
         if new_devices:
-            tasks.append(AlexaAPI.get_users_me(login_obj))
+            tasks.append(AlexaAPI.get_authentication(login_obj))
 
         entities_to_monitor = set()
         for sensor in hass.data[DATA_ALEXAMEDIA]["accounts"][email]["entities"][
@@ -532,8 +532,7 @@ async def setup_alexa(hass, config_entry, login_obj: AlexaLogin):
                     entity_state = optional_task_results.pop()
 
                 if new_devices:
-                    users_me = optional_task_results.pop()
-                    auth_info = {"customerId": users_me.get("id")}
+                    auth_info = optional_task_results.pop()
                     _LOGGER.debug(
                         "%s: Found %s devices, %s bluetooth",
                         hide_email(email),
